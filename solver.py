75d0e5c490fa2b329b7abba6340b388079cfe69d
Jing Qiang <tohjingqiang@gmail.com>
2024-10-22 15:04:18 +0800

    add multi-processing

--- solver.py
+++ solver.py
@@ -14,7 +14,8 @@ def train(
         val_dataloader: DataLoader,
         min_epoch: int = 20,
         max_epoch: int = 10_000,
-        max_non_increasing_epoch_count: int = 10
+        max_non_increasing_epoch_count: int = 10,
+        show_progress: bool = True
     ):
     """
         Model training step.
@@ -39,8 +40,8 @@ def train(
     for epoch in range(1, max_epoch+1):
         num_of_epochs += 1
 
-        epoch_train_loss, epoch_train_acc = __train_one_epoch(model, train_dataloader, optimizer, criterion, epoch)
-        epoch_val_loss, epoch_val_acc = __validate(model, criterion, val_dataloader, epoch)
+        epoch_train_loss, epoch_train_acc = __train_one_epoch(model, train_dataloader, optimizer, criterion, epoch, show_progress)
+        epoch_val_loss, epoch_val_acc = __validate(model, criterion, val_dataloader, epoch, show_progress)
         
         avg_train_loss.append(epoch_train_loss)
         avg_train_acc.append(epoch_train_acc)
@@ -48,7 +49,7 @@ def train(
         avg_val_acc.append(epoch_val_acc)
 
         # Check if epoch validation accuracy score is increasing.
-        # Stop model training when validation accuracy does not increase for a few epochs (starting from 20 epochs onwards)
+        # Stop model training when validation accuracy does not increase for a few epochs (starting from `min_epoch` onwards)
         window_size = max(non_increasing_epoch_count, max_non_increasing_epoch_count)
         recent_highest_seen = max(avg_val_acc[-2: -2-window_size: -1]) if num_of_epochs >= window_size else 0 # Maximum validation accuracy seen in the recent window
         if num_of_epochs >= min_epoch and avg_val_acc[-1] <= recent_highest_seen:
@@ -67,7 +68,8 @@ def __train_one_epoch(
         train_dataloader: DataLoader,
         optimizer,
         criterion,
-        epoch=0
+        epoch=0,
+        show_progress: bool = True
     ):
     model.train()
     
@@ -75,29 +77,34 @@ def __train_one_epoch(
     train_loss, train_acc = [], []
     total_step = 0
     
-    with tqdm(total=total) as t:
+    if show_progress:
+        t = tqdm(total=total)
         t.set_description('Epoch %i (Train)' % epoch)
 
-        # Mini-Batch
-        for i, (sentences, labels) in enumerate(train_dataloader):
-            optimizer.zero_grad()
-            predictions = model(sentences)
+    # Mini-Batch
+    for i, (sentences, labels) in enumerate(train_dataloader):
+        optimizer.zero_grad()
+        predictions = model(sentences)
 
-            loss = criterion(predictions, labels)
-            loss.backward()
-            optimizer.step()
-            
-            # loss
-            train_loss.append(loss.detach().numpy())
+        loss = criterion(predictions, labels)
+        loss.backward()
+        optimizer.step()
+        
+        # loss
+        train_loss.append(loss.detach().numpy())
 
-            # compute accuracy
-            acc = __binary_accuracy(predictions, labels)
-            train_acc.append(acc)
-            
-            total_step +=1
+        # compute accuracy
+        acc = __binary_accuracy(predictions, labels)
+        train_acc.append(acc)
+        
+        total_step +=1
+        if show_progress:
             t.set_postfix(loss=np.mean(train_loss), acc=np.mean(train_acc))
             t.update(1)
-            
+    
+    if show_progress:
+        t.close()
+
     return np.mean(train_loss), np.mean(train_acc)
 
 def __binary_accuracy(predictions, labels):
@@ -116,7 +123,8 @@ def __validate(
         model, 
         criterion, 
         val_dataloader: DataLoader,
-        epoch=0
+        epoch=0,
+        show_progress: bool = True
     ):
     model.eval()
     
@@ -125,22 +133,28 @@ def __validate(
     total_step = 0
     
     with torch.no_grad():
-        with tqdm(total=total) as t:
+        if show_progress:
+            t = tqdm(total=total)
             t.set_description('Epoch %i (Val)' % epoch)
-            for i, (sentences, labels) in enumerate(val_dataloader):
-                predictions = model(sentences)
-                
-                # compute loss
-                loss = criterion(predictions, labels)
-                val_loss.append(loss.detach().numpy()) 
-                
-                # compute accuracy
-                acc = __binary_accuracy(predictions, labels)
-                val_acc.append(acc)
-                
-                total_step +=1
+
+        for i, (sentences, labels) in enumerate(val_dataloader):
+            predictions = model(sentences)
+            
+            # compute loss
+            loss = criterion(predictions, labels)
+            val_loss.append(loss.detach().numpy()) 
+            
+            # compute accuracy
+            acc = __binary_accuracy(predictions, labels)
+            val_acc.append(acc)
+            
+            total_step +=1
+            if show_progress:
                 t.set_postfix(loss=np.mean(val_loss), acc=np.mean(val_acc))
                 t.update(1)
+        
+        if show_progress:
+            t.close()
             
     return np.mean(val_loss), np.mean(val_acc)
 
