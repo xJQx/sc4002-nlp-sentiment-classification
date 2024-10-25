d569c18e786495bbb5f991331365dffd05a8984b
Jing Hua <tohjinghua123@gmail.com>
2024-10-25 19:30:42 +0800

    add token_to_index

--- utils/text.py
+++ utils/text.py
@@ -36,8 +36,32 @@ def tokenize(
     return dataset.filter(filter_na).map(_tokenize, batched=False)
 
 
+def token_to_index(
+    dataset: Dataset,
+    index_from_word: dict,
+    keep_oov_token: bool = False,
+) -> Dataset:
+    def _token_to_index(example):
+        indexes = []
+        sentence_tokens = example["tokens"]
+
+        for token in sentence_tokens:
+            index = index_from_word.get(token, None)
+
+            if index or keep_oov_token:
+                indexes.append(index)
+
+        return {"indexes": indexes}
+
+    return dataset.map(_token_to_index)
+
+
 def get_context_average_embedding(
-    sentence_tokens: list[str], oov_token: str, w2v_model: any
+    sentence_tokens: list[str],
+    oov_token: str,
+    w2v_model: any = None,
+    embedding_matrix: np.ndarray = None,
+    index_from_word: dict = None,
 ) -> np.ndarray:
     """Generates an approximate embedding for an out-of-vocabulary (OOV) word by averaging the
     embeddings of surrounding context words within the sentence.
@@ -47,6 +71,8 @@ def get_context_average_embedding(
         oov_token (str): The OOV word for which an approximate embedding is needed.
         w2v_model (gensim.models.KeyedVectors): A pretrained Word2Vec model where word embeddings
             can be accessed by `w2v_model[word]`.
+        embedding_matrix (np.ndarray, optional): Precomputed embedding matrix.
+        index_from_word (dict, optional): Dictionary mapping words to row indices in the embedding matrix.
 
     Returns:
         np.ndarray: The averaged embedding vector for the OOV word based on its context.
@@ -63,10 +89,23 @@ def get_context_average_embedding(
         This function assumes that `sentence_tokens` is already tokenized and in lowercase.
     """
 
+    def _get_embedding(word: str) -> np.ndarray:
+        """Generates embedding with w2v model / embedding matrix."""
+        if w2v_model and word in w2v_model.key_to_index:
+            return w2v_model[word]
+        elif (
+            embedding_matrix is not None
+            and index_from_word is not None
+            and word in index_from_word
+        ):
+            return embedding_matrix[index_from_word[word]]
+
+        return None
+
     context_embeddings = [
-        w2v_model[word]
+        _get_embedding(word)
         for word in sentence_tokens
-        if word != oov_token and word in w2v_model.key_to_index
+        if word != oov_token and _get_embedding(word) is not None
     ]
     if context_embeddings:
         return np.mean(context_embeddings, axis=0)  # Average embedding of context
